ba2aa0e00823ed5c6df4f777109de7b02faadfd3
Thiagocolombosilva <36051222+Thiagocolombosilva@users.noreply.github.com>
2025-10-17 10:22:33 -0300

    Update HeroImage.jsx

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -5,10 +5,11 @@ const HeroImage = () => {
     <div className='flex justify-center items-center'>
       <img 
         src='https://imagedelivery.net/LqiWLm-3MGbYHtFuUbcBtA/119580eb-abd9-4191-b93a-f01938786700/public' 
-        alt='Hostinger Horizons' 
+        alt='Raspshot' 
       />
     </div>
   );
 };
+
 
 export default HeroImage;
